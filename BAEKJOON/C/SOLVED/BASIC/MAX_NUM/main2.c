2b569bf255aa1ceb535c172b1ef9a2b67a1779b0
jerry901 <pkhu2000@gmail.com>
2019-10-15 19:17:36 +0900

    trying pushing on vscode

--- BAEKJOON/C/SOLVED/BASIC/MAX_NUM/main2.c
+++
@@ -1,4 +0,0 @@
-main(a,b,c){
-	scanf("%d%d%d",&a,&b,&c);
-	b=a>b>c?a:b:c;
-	printf("%d",b);}
--- BAEKJOON/C/SOLVED/BFS/MAKE_ONE/mm.c
+++
@@ -1,1 +0,0 @@
-nt f(int k){int a,b;if(k<2)return 0;a=f(k/2)+k%2+1;b=f(k/3)+k%3+1;if(a<b)return a;else return b;}main(n){scanf("%d",&n);printf("%d\n",f(n));}
