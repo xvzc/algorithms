13efe045d30855c5f3ba2e988f2d6b48bef2f067
jerry901 <pkhu2000@gmail.com>
2021-05-08 22:01:12 +0900

    PS

---
+++ baekJoon/python/input.py
@@ -0,0 +1,9 @@
+import sys
+
+sysin = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+t=[int(sysin()) for _ in range(n)]
+wines=[int(sysin()) for _ in range(int(sysin()))]
---
+++ baekJoon/python/test.py
@@ -0,0 +1,4 @@
+from sys import stdin
+
+n = int(input());t=[int(stdin.readline()) for _ in range(n)];a,b,c = 0,0,0
+print(t)
