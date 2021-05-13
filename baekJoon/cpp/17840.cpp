a14feef124468208d58814807a74cd97df686723
jerry901 <pkhu2000@gmail.com>
2021-05-13 19:46:06 +0900

    ps

--- baekJoon/cpp/17840.cpp
+++ baekJoon/cpp/17840.cpp
@@ -1,19 +1,16 @@
 // #define SUBMIT
-
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 
 #define endl '\n'
 #define ll long long
 #define MAX 100
 
-int T[100001];
-int rem;
+int Q; // Test case
+int M; // remainder
+int N;
 
-int dp[MAX] = {0, 1, 1};
-int dp_digits[5] = {0, };
-
+ll dp[MAX] = {0, 1, 1};
+ll dp_mod[MAX] = {0, 1, 1};
 
 using namespace std;
 
@@ -24,21 +21,11 @@ int main(int argc, char const *argv[]){
 #endif
 
     ios_base::sync_with_stdio(false); cin.tie(NULL);
-    
+
 
     for(int i = 3; i < MAX; ++i){
-        dp[i] = (dp[i-1] + dp[i-2])%1000;
-
-        if(dp[i] == 1000)
-            dp_digits[4]++;
-        else if(dp[i] >= 100)
-            dp_digits[3]++;
-        else if(dp[i] >= 10)
-            dp_digits[2]++;
-        else
-            dp_digits[1]++;
-
-
+        dp[i] = dp[i-1] + dp[i-2];
+        dp_mod[i] = (dp_mod[i-1]%5 + dp_mod[i-2]%5)%5;
     }
      /* code */
 
@@ -47,7 +34,7 @@ int main(int argc, char const *argv[]){
     for(auto const& value : dp)
         cout << value << " "; 
     cout << endl;
-    for(auto const& value : dp_digits)
+    for(auto const& value : dp_mod)
         cout << value << " "; 
     cout << endl;
     
