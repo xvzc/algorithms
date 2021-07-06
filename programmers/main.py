06400d2c9119c5e851be21c7b26f8a62392c42cd
jerry901 <pkhu2000@gmail.com>
2021-07-06 18:05:46 +0900

    resolve

--- programmers/main.py
+++ programmers/main.py
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-def solution(clothes):
-    answer = 0
-    hmap={}
-    for cloth in clothes:
-        if cloth[1] not in hmap:
-            hmap[cloth[1]]=[]
-            hmap[cloth[1]].append(cloth[0])
-        else:
-            hmap[cloth[1]].append(cloth[0])
-
-    print(hmap)
-
-    return answer
-
-
-clothes=[["yellowhat", "headgear"], ["bluesunglasses", "eyewear"], ["green_turban", "headgear"]]
-solution(clothes)
-
-
-=======
 def solution(absolutes, signs):
     answer = 0
     for i in range(len(absolutes)):
@@ -30,4 +9,3 @@ def solution(absolutes, signs):
 absolutes=[4,7,12]
 signs=[True,False,True]
 print(solution(absolutes, signs))
->>>>>>> 700aff5e1c056b96d192285e5ad09ca678c24d20
