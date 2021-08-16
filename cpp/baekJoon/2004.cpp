59b98ae2d1411a954cea799f0be6194355070609
jerry901 <pkhu2000@gmail.com>
2021-08-17 02:27:24 +0900

    [UPDATE] solve

--- cpp/baekJoon/2004.cpp
+++ cpp/baekJoon/2004.cpp
@@ -16,19 +16,38 @@ using namespace std;
 // *********************************
 
 // FUNCTIONS ***********************
-void solution() {}
+ll num_of_five(ll num) {
+    ll ret = 0;
+    for(ll i=5; i <= num; i=i*5) {
+        ret+=num/i;
+    }
+    return ret;
+}
+
+ll num_of_two(ll num) {
+    ll ret = 0;
+    for(ll i=2; i <= num; i=i*2) {
+        ret+=num/i;
+    }
+    return ret;
+}
 // *********************************
 
-// #define SUBMIT
+// #define TEST
 int main() {
-    io_faster;
+	io_faster;
 
-#ifndef SUBMIT
-    cout << "# From the test input " << endl;
-    from_test_case;
-#endif
+	#ifdef TEST
+	from_test_case;
+	#endif
 
-	solution();
+    ll n, m;
+    cin >> n >> m;
+
+    cout << min(num_of_five(n) - ( num_of_five(m) + num_of_five(n-m) ), num_of_two(n) - ( num_of_two(m)+num_of_two(n-m) ) );
 
     return 0;
 }
+
+
+
