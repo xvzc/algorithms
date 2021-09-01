5f38fa65c3c2c7bd136eae476a53aa9da312a52a
jerry901 <pkhu2000@gmail.com>
2021-09-01 14:59:18 +0900

    solve

---
+++ cpp/baekJoon/10866.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+#define io_faster ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define from_test_case (void)!freopen("input.txt", "r", stdin)
+#define endl '\n'
+
+#define fi first
+#define se second
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace std;
+
+// GLOBAL VARIABLES ****************
+// *********************************
+
+// FUNCTIONS ***********************
+void solution() {}
+// *********************************
+
+// #define SUBMIT
+int main() {
+    io_faster;
+
+#ifndef SUBMIT
+    cout << "# From the test input " << endl;
+    from_test_case;
+#endif
+
+	solution();
+
+    return 0;
+}
+
