32ea52dfcf9c1a0bfd7b4263477e417f4e972af2
jerry901 <pkhu2000@gmail.com>
2021-09-01 15:34:06 +0900

    solve

--- cpp/baekJoon/10866.cpp
+++ cpp/baekJoon/10866.cpp
@@ -19,7 +19,7 @@ using namespace std;
 void solution() {}
 // *********************************
 
-// #define SUBMIT
+#define SUBMIT
 int main() {
     io_faster;
 
@@ -27,6 +27,55 @@ int main() {
     cout << "# From the test input " << endl;
     from_test_case;
 #endif
+    deque<int> dq;
+    int N, x;
+    string s;
+    cin >> N;
+    while(N--) {
+        cin >> s;
+        if (s[1] == 'u') { // push_
+            cin >> x;
+            if (s[5] == 'b'){ // back
+                dq.push_back(x);
+            } else if (s[5] == 'f') { // front
+                dq.push_front(x);
+            }
+        } else if (s[1] == 'o') { // pop_
+            int temp;
+            if (s[4] == 'b'){ // back
+                if (dq.empty()) {
+                    temp = -1;
+                } else {
+                    temp = dq.back();
+                    dq.pop_back();
+                }
+            } else if (s[4] == 'f') { // front
+                if (dq.empty()) {
+                    temp = -1;
+                } else {
+                    temp = dq.front();
+                    dq.pop_front();
+                }
+            }
+            cout << temp << endl;
+        } else if (s[0] == 'f') {
+            if (dq.empty()) {
+                cout << -1 << endl;
+            } else {
+                cout << dq.front() << endl;
+            }
+        } else if (s[0] == 'b') {
+            if (dq.empty()) {
+                cout << -1 << endl;
+            } else {
+                cout << dq.back() << endl;
+            }
+        } else if (s[0] == 's') {
+            cout << dq.size() << endl;
+        } else if (s[0] == 'e') {
+            cout << dq.empty()  << endl;
+        }
+    } 
 
 	solution();
 
