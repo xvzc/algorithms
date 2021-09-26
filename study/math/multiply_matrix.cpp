ab64d67f564c70bad30516cfb68f115aea38d246
jerry901 <pkhu2000@gmail.com>
2021-09-26 13:50:41 +0900

    update readme

--- study/math/multiply_matrix.cpp
+++ study/math/multiply_matrix.cpp
@@ -2,13 +2,13 @@
 
 using namespace std;
 
-vector<vector<int>> multiply_matrix(vector<vector<int>> v1, vector<vector<int>> v2, int mod) {
-    vector<vector<int>> ret(v1[0].size(), vector<int>(v1.size(), 0));
-    int n = v1.size();
+vector<vector<int>> multiply_matrix(vector<vector<int>> m1, vector<vector<int>> m2, int mod) {
+    vector<vector<int>> ret(m1[0].size(), vector<int>(m1.size(), 0));
+    int n = m1.size();
     for(int i = 0; i < n; ++i) {
         for(int j = 0; j < n; ++j) {
             for(int k = 0; k < n; ++k) {
-                ret[i][j] = ((ret[i][j] % mod) + (((long long)v1[i][k] % mod) * ((long long)v2[k][j] % mod)) % mod) % mod;
+                ret[i][j] = ((ret[i][j] % mod) + (((long long)m1[i][k] % mod) * ((long long)m2[k][j] % mod)) % mod) % mod;
             }
         }
     }
