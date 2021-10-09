41251ae790794041b2125ab442c45f2bc5af0b14
jerry901 <pkhu2000@gmail.com>
2021-10-09 20:47:38 +0900

    solve

---
+++ cpp/baekJoon/solved/2845.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+#define improve_io ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define from_test_case (void)!freopen("input.txt", "r", stdin)
+#define endl '\n'
+
+#define fi first
+#define se second
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace std;
+
+/* - GLOBAL VARIABLES ---------------------------- */
+
+/* ----------------------------------------------- */
+
+/* - FUNCTIONS ----------------------------------- */
+
+/* ----------------------------------------------- */
+
+#define SUBMIT
+int main() {
+    improve_io;
+
+#ifndef SUBMIT
+    cout << "# From the test input " << endl;
+    from_test_case;
+#endif
+    int a, b; cin >> a >> b;
+    int total = a * b;
+
+    int temp;
+    for(int i = 0; i < 5; ++i) {
+        cin >> temp;
+        cout << temp - total << ' ';
+    }
+
+
+
+    return 0;
+}
+
