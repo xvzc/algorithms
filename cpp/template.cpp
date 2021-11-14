61d819b74aec618a90ba04f462e904a12b071a88
jerry901 <pkhu2000@gmail.com>
2021-11-14 15:19:31 +0900

    update template

--- cpp/template.cpp
+++ cpp/template.cpp
@@ -1,9 +1,16 @@
 #include <bits/stdc++.h>
 
+#define SUBMIT
+#define debug if constexpr (!submit) std::cout
 #define endl '\n'
-
 #define fi first
 #define se second
+
+#ifdef SUBMIT
+constexpr bool submit = true;
+#else
+constexpr bool submit = false;
+#endif
 
 typedef long long ll;
 typedef unsigned long long ull;
@@ -11,22 +18,20 @@ typedef unsigned long long ull;
 using namespace std;
 
 /* - GLOBAL VARIABLES ---------------------------- */
-
+ 
 /* ----------------------------------------------- */
 
 /* - FUNCTIONS ----------------------------------- */
 
 /* ----------------------------------------------- */
-
-// #define SUBMIT
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
 
-#ifndef SUBMIT
-    (void)!freopen("input.txt", "r", stdin);
-    cout << "# From the test case" << endl;
-#endif
+    if constexpr (!submit) {
+        (void)!freopen("input.txt", "r", stdin);
+        cout << "# From the test case" << endl;
+    }
 
     return 0;
 }
