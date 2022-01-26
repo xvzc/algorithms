1506590ed3b7ed13f254848625ac870d2d175f05
jerry901 <pkhu2000@gmail.com>
2022-01-26 21:36:41 +0900

    scratch

--- cpp/baekJoon/18320.cpp
+++ cpp/baekJoon/18320.cpp
@@ -17,8 +17,7 @@ typedef unsigned long long ull;
 using namespace std;
 
 /* - GLOBAL VARIABLES ---------------------------- */
-const ll MAX = 1000000000000;
-ll N, K, M, G, X;
+ll N, K, M;
 /* ----------------------------------------------- */
 
 /* - FUNCTIONS ----------------------------------- */
@@ -34,64 +33,16 @@ int main() {
 
     cin >> N >> K >> M;
 
-    ll xl = 0; 
-    ll xr = MAX;
-    ll xm = xl + xr / 2;
-    ll X = LLONG_MAX;
-    while (xl < xr) {
-        ll gl = 0;
-        ll gr = MAX; 
-        ll gm = gl + gr / 2;
-        ll G = LLONG_MAX;
-        while (gl < gr) {
-            ll gY = ((double)(N - gm) / xm);
-            gY = gY < M ? M : gY;
-            ll gdays = (N - gm) / gY;
+    ll daily_amount, days;
 
-            if ((N - gm) % gY) {
-                gdays++;
-            }
+    ll g_left = 1, g_right = N, g_mid, G = N + 1;
+    while (g_left <= g_right) {
 
-            if (gdays < K) {
-                gr = gm - 1;
-            }
-
-            if (gdays >= K) {
-                gl = gm + 1;
-            }
-
-            if (gdays >= K && gm < G) {
-                G = gm;
-            }
-
-            gm = gr + gl / 2;
+    ll x_left = 1, x_right = N, x_mid, X = N + 1;
+        while (x_left <= x_right) {
+            x_mid = x_left + (r)
         }
 
-        if (G == LLONG_MAX) {
-            continue;
-        }
-
-        ll xY = ((double)(N - G) / xm);
-        xY = xY < M ? M : xY;
-        ll xdays = (N - G) / xY;
-
-        if ((N - G) % xY) {
-            xdays++;
-        }
-
-        if (xdays < K) {
-            xl = xm + 1;
-        }
-
-        if (xdays >= K) {
-            xr = xm - 1;
-        }
-
-        xm = xl + xr / 2;
-
-        if (xdays >= K && xm < X) {
-            X = xm;
-        }
     }
 
     cout << X << endl;
